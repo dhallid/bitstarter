eb81049c62c0bd28f314350d96a3509fd8792bf0
dhallid <ubuntu@domU-12-31-39-06-DA-B6.compute-1.internal>
2013-07-18 01:30:01 +0000

    prepare for HW#4

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
 var myBuffer = fs.readFileSync("index.html");
 
+app.use(express.favicon("images/favicon.ico"));
 app.get('/', function(request, response) {
     response.send(myBuffer.toString("utf8"));
 });
